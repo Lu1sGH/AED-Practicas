b63be41e6b78a1e2a76bde10bd280aee868a160b
Lu1sGH <99305390+Lu1sGH@users.noreply.github.com>
2024-06-10 23:57:16 -0600

    2 v3

--- 2.c
+++ 2.c
@@ -6,29 +6,31 @@ void hacerPliegues();
 void pirueta(int);
 
 int main(){
-  
-    int a[7] = {1, 2, 3, 4, 5, 6, 7};
+    int N = 6;
+    int a[6] = {0};
     int b[3] = {0};
-    lanzar(a, 0, 6, b, 5);
+    lanzar(a, 0, N-1, b, 5);
 
     return 0;
 }
 
 void lanzar(int *torre, int m, int n, int *fran, int M){
-    if(m>n) return;
     int i,j, par;
-    
-    par = (n-m+1)/6;
-
-    for(i=1; i <= 6; i++){
-        lanzar(torre, m+(i-1)*par+1, m+(i)*par, fran, M);
-        hacerPliegues();
-    }
     
     if(m==n){
         for(j = 0; j < M; j++) pirueta(torre[m]);
         fran[rand()%3] = torre[m];
+        return;
     }
+    
+    par = (n-m+1)/6;
+
+    for(i=1; i <= 6; i++){
+        lanzar(torre, m + (i-1)*par, m + i*par, fran, M);
+        hacerPliegues();
+    }
+    
+    
 }
 
 void hacerPliegues(){
